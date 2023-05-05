374a2cb803d54a604b1c2430c5dbdd2ce92f2cc7
nathan <nathanmageka6@gmail.com>
2023-05-05 23:14:30 +0300

    nathan

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Item({ name, category }) {
+function Item({ name, category }) { 
+
+  const handleClick = () => {
+    setInCart(true);
+  };
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add"onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
